446190df0954e5d5cca03990aba654ad9cc84f58
Julie Boronenko <boronyul@fit.cvut.cz>
2018-05-20 16:41:45 +0200

    Global changes with 1) Class Cell - moving all type of methods getValue() to every inherited class, adding virtual class to Cell ToString(); 2)finally making 2D array of pointers on Cell! 3) decision to use extra, not yet implemented libraries (like libncurses). Just wish me Good luck!

--- Model.cpp
+++ Model.cpp
@@ -2,6 +2,13 @@
 // Created by julinka on 2.5.18.
 //
 #include "Model.h"
+#include "Bool.h"
+#include "Reference.h"
+#include "Number.h"
+#include "Text.h"
+#include "Expression.h"
+
+Model *Model::instance = 0;
 
 unsigned Model::getHeight() const {
     return height;
@@ -11,35 +18,90 @@ unsigned Model::getWidth() const {
     return width;
 }
 
-Model::Model(): width(20), height(30) {
+Model::Model() {
+    width = 10;
+    height = 25;
     spreadSheet.resize(height);
-    for( int i = 0; i < height; ++i )
+    for (int i = 0; i < height; ++i)
         spreadSheet[i].resize(width);
-//    for( int i = 0; i < height; ++i ){
-//        for( int t = 0; t < width; ++t )
-//            spreadSheet[i][t] = new Cell;
-//    }
+    for (int i = 0; i < height; ++i)
+        for (int j = 0; j < width; ++j)
+            spreadSheet[i][j] = nullptr;
 }
 
-Model::Model(const int &width, const int &height): width(width), height(height) {
-    spreadSheet.resize(height);
-    for( int i = 0; i < height; ++i )
-        spreadSheet[i].resize(width);
-
-    for( int i = 0; i < height; ++i ){
-        for( int t = 0; t < width; ++t )
-            spreadSheet[i][t] = nullptr;
-    }
-}
 Model::~Model() {
-    for( int i = 0; i < height; ++i ){
-        for( int t = 0; t < width; ++t )
-            if( spreadSheet[i][t] )
+    for (int i = 0; i < getHeight(); ++i) {
+        for (int t = 0; t < width; ++t)
+            if (spreadSheet[i][t])
                 delete spreadSheet[i][t];
     }
 }
 
-const Cell * Model::getElement(const int & height, const int & width) const{
+
+const Cell *Model::getElement(const int &height, const int &width) const {
     return spreadSheet[height - 1][width];
 
 }
+
+Model *Model::getInstance() {
+    if (instance == 0) {
+        instance = new Model();
+    }
+
+    return instance;
+}
+
+void Model::setWidth(unsigned int width) {
+    Model::width = width;
+}
+
+void Model::setHeight(unsigned int height) {
+    Model::height = height;
+}
+
+void Model::setValue(const int &xCoor, const int &yCoor, const Cell *cell) {
+    switch (cell->getType()) {
+        case CellType::BOOLEAN : {
+            spreadSheet[yCoor][xCoor] = new Bool(*cell); // todo kopirujici konstruktor
+            break;
+        }
+        case CellType::REFERENCE : {
+            spreadSheet[yCoor][xCoor] = new Reference(*cell);
+            break;
+        }
+        case CellType::NUMBER : {
+            spreadSheet[yCoor][xCoor] = new Number(*cell);
+            break;
+        }
+        case CellType::TEXT: {
+            spreadSheet[yCoor][xCoor] = new Text(*cell);
+            break;
+        }
+        case CellType::EXPRESSION: {
+            spreadSheet[yCoor][xCoor] = new Expression(*cell);
+            break;
+        }
+        case CellType::OPERATION: {
+            spreadSheet[yCoor][xCoor] = new Operation(*cell); // todo class Operation + kopirujici konstruktor
+            break;
+        }
+    }
+}
+
+void Model::resizeTable(const int &xCoor, const int &yCoor) {
+    int previousWidth = width;
+    int previousHeight = height;
+    height = (unsigned) yCoor;
+    width = (unsigned) xCoor;
+
+    spreadSheet.resize((unsigned long) yCoor);
+    for (int i = 0; i < height; ++i)
+        spreadSheet[i].resize((unsigned long) xCoor);
+
+    for (int i = 0; i < height; ++i) {
+        for (int t = previousWidth; t < width; ++t)
+            spreadSheet[i][t] = nullptr;
+    }
+}
+
+
